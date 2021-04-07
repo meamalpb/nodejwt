87d8fc9327f4cde1cc7115cb1bcf20f77cab58ca
Amal PB <63001461+meamalpb@users.noreply.github.com>
2021-04-08 00:23:47 +0530

    Update index.js

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const cookieParser    =require('cookie-parser');
 const {requireAuth}   = require('./middleware/authMiddleware');
 
 //connect to mongodb cloud
-const uri = "mongodb+srv://pbamal:test123@cluster0.trivh.mongodb.net/test?retryWrites=true&w=majority";
+const uri = "mongodb+srv://<username>:<password>@cluster0.trivh.mongodb.net/test?retryWrites=true&w=majority";
 mongoose.connect(uri, { useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex:true })
   .then((result) => app.listen(3000,()=>{console.log('http://localhost:3000/')}))
   .catch((err) => console.log(err));
